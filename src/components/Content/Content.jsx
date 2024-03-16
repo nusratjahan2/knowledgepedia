7263cbe33470c2e406a1b2e43d5d9df18a3b489d
unknown <nusrathaque717@gmail.com>
2024-03-16 06:15:10 +0600

    first commit

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Content.css';
+
+const Content = () => {
+    return (
+        <div>
+          <div className='blog'>
+            
+          </div>
+          <div className='bookmarked'>
+            
+          </div>
+        </div>
+    );
+};
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+   return (
+       <div className='navDesign'>
+         <h1>Knowledgepedia</h1>
+        <img src=' https://images.unsplash.com/photo-1707672972137-64390186af62?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNDN8fHxlbnwwfHx8fHw%3D' alt='' />
+       </div>
+   );
+};
+
+export default Navbar;
